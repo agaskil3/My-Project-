d4785bf6ff163587e39204f04c4eef67f489bb2b
Andrea Gaskill <agaskil3@agaskil3-VirtualBox.(none)>
2012-03-06 19:32:23 -0600

    worked on views, added templates

--- settings.py
+++ settings.py
@@ -103,9 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+        "/home/agaskil3/myproject/templates"
 )
 
 INSTALLED_APPS = (
